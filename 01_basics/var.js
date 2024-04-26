e6782fa2316e1a3f97030850f46c12830d3485f3
md daniyal <140130748+DaniyalAman@users.noreply.github.com>
2024-04-26 21:10:20 +0000

    container  config done

---
+++ 01_basics/var.js
@@ -0,0 +1,15 @@
+const UserName="Daniyal"
+let UserID ="Danny@123";
+var EmailId="aman@123";
+let Password="123455";
+
+let city;
+
+console.table([UserName, UserID, EmailId, Password,city]);
+
+/*
+Note:-
+prefer we not use var:-
+Becoz of issues block scope and function scope
+{}:-it called scope ;
+*/
