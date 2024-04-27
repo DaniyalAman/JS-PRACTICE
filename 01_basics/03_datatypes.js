31ce3182004bd0507264e554f94ed5ebe61dadb4
md daniyal <140130748+DaniyalAman@users.noreply.github.com>
2024-04-27 17:52:01 +0000

    datatypes

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,23 @@
+// "use strict" :- treate a all js in a new version
+
+// alert(3+3):- we always use in node js not a browser
+
+// console.log("daniyal"); :- it is not a good practice and code readability
+// console.log(3+3); be  high
+
+let name="daniyal";
+let age = 23;
+let loggedIn =true;
+
+console.log((typeof "daniyal"));
+console.log((typeof age));
+
+// num: 33
+// BigInt:
+// string: "aman"
+// Boolean: true/false
+// null : empty / object
+// undefined : not a asign a value
+// NaN :
+// Symbol: whenever talk in symbol it is represent in unique value 
+// object :
