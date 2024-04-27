ae6c951f8eb8f607962957dccc1e76b0b7396fdd
md daniyal <140130748+DaniyalAman@users.noreply.github.com>
2024-04-27 18:12:00 +0000

    conversion and operation

---
+++ 01_basics/02_ConversionAndOpration.js
@@ -0,0 +1,15 @@
+//  let score = 33;
+// // let score = "33aa"; value  NaN
+// // let score = true; value  1
+// // let score = false; value  0
+// console.log(typeof score);
+
+// let valueINumber = Number(score);
+
+// console.log(typeof valueINumber);
+
+// let isLoggedIn =1
+let isLoggedIn = "danny";
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
